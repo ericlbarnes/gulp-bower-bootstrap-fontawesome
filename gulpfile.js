466d774642c92d474d8ecfb0bb1928e8032cfc94
Muhammed Athimannil <hello@athimannil.com>
2015-10-14 11:47:48 +0400

    fixes
    bootstrap-sass 3.3.5
    gulp-ruby-sass to gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,35 @@
-var gulp = require('gulp'), 
-    sass = require('gulp-ruby-sass') 
-    notify = require("gulp-notify") 
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    notify = require("gulp-notify"),
     bower = require('gulp-bower');
 
 var config = {
-     sassPath: './resources/sass',
-     bowerDir: './bower_components' 
-}
+    sassPath: './resources/sass',
+    bowerDir: './bower_components'
+};
 
-gulp.task('bower', function() { 
+gulp.task('bower', function() {
     return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
+        .pipe(gulp.dest(config.bowerDir));
 });
 
-gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('./public/fonts')); 
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/fonts'));
 });
 
-gulp.task('css', function() { 
+gulp.task('css', function() {
     return gulp.src(config.sassPath + '/style.scss')
-         .pipe(sass({
-             style: 'compressed',
-             loadPath: [
-                 './resources/sass',
-                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
+        .pipe(sass()
             .on("error", notify.onError(function (error) {
-                 return "Error: " + error.message;
-             }))) 
-         .pipe(gulp.dest('./public/css')); 
+                return "Error: " + error.message;
+            })))
+        .pipe(gulp.dest('./public/css'));
 });
 
 // Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-  gulp.task('default', ['bower', 'icons', 'css']);
+gulp.task('default', ['bower', 'icons', 'css']);
